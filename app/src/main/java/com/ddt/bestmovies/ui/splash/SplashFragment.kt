9dd9f5ed92e419ce15c1bd98de8d5af9107bfcf7
Mohammad Yazdi <mohammadyt1379@gmail.com>
2022-07-29 01:51:52 +0430

    complete SplashFragment.kt

--- app/src/main/java/com/ddt/bestmovies/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/ddt/bestmovies/ui/splash/SplashFragment.kt
@@ -5,17 +5,52 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.coroutineScope
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.ddt.bestmovies.R
 import com.ddt.bestmovies.databinding.FragmentSplashBinding
+import com.ddt.bestmovies.utils.Constants
+import com.ddt.bestmovies.utils.UserDataStore
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SplashFragment : Fragment() {
 
     //Binding
     private lateinit var binding: FragmentSplashBinding
+    //dataSore
+    @Inject
+    lateinit var userDataStore: UserDataStore
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding= FragmentSplashBinding.inflate(layoutInflater,container,false)
         return binding.root
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //Set delay
+        lifecycle.coroutineScope.launchWhenCreated {
+            delay(Constants.SPLASH_DELAY_DURATION)
+
+            //check user token
+            userDataStore.getUserToken().collect(){
+                if(it.isEmpty())
+                    findNavController().navigate(R.id.actionSplashToRegister)
+                else
+                    findNavController().navigate(R.id.actionSplashToHome)
+
+            }
+
+        }
     }
 
 }
--- app/src/main/java/com/ddt/bestmovies/utils/Constants.kt
+++ app/src/main/java/com/ddt/bestmovies/utils/Constants.kt
@@ -2,6 +2,11 @@ package com.ddt.bestmovies.utils
 
 object Constants {
 
+    //dataSore
     const val USER_INFO_DATA_STORE="user_info_data_store"
     const val USER_TOKEN_KEY="user_token_key"
+
+    //SplashFragment
+    const val SPLASH_DELAY_DURATION=2000L
+
 }
