19834131c6cf15d7b77e4078081c4977160290e1
Mohammad Yazdi <mohammadyt1379@gmail.com>
2022-07-28 20:57:41 +0430

    design SplashFragment

--- app/src/main/java/com/ddt/bestmovies/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/ddt/bestmovies/ui/splash/SplashFragment.kt
@@ -6,13 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ddt.bestmovies.R
+import com.ddt.bestmovies.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
 
+    //Binding
+    private lateinit var binding: FragmentSplashBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        binding= FragmentSplashBinding.inflate(layoutInflater,container,false)
+        return binding.root
     }
 
 }
