f4472d3f6e90dc68d16444abdbc82815774537e7
Mohammad Yazdi <mohammadyt1379@gmail.com>
2022-07-29 23:31:09 +0430

    create RegisterViewModel.kt

---
+++ app/src/main/java/com/ddt/bestmovies/viewmodel/RegisterViewModel.kt
@@ -0,0 +1,35 @@
+package com.ddt.bestmovies.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ddt.bestmovies.models.register.BodyRegister
+import com.ddt.bestmovies.models.register.ResponseRegister
+import com.ddt.bestmovies.repository.RegisterRepository
+import dagger.hilt.android.scopes.ViewModelScoped
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@ViewModelScoped
+class RegisterViewModel @Inject constructor(private val registerRepository: RegisterRepository) : ViewModel() {
+
+    val responseRegister=MutableLiveData<ResponseRegister>()
+    val loading=MutableLiveData<Boolean>()
+
+    fun registerUser(bodyRegister: BodyRegister){
+
+        viewModelScope.launch {
+            loading.postValue(true)
+
+            val response=registerRepository.registerUser(bodyRegister)
+            if(response.isSuccessful){
+                responseRegister.postValue(response.body())
+            }
+            loading.postValue(false)
+
+
+        }
+    }
+
+
+}
